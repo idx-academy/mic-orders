97409cf066db83c6d48fd9664988ad9e1fe73c3e
Yan <yanchumak@gmail.com>
2024-06-17 23:58:16 +0200

    Updated application-secrets.yml

--- code/orders-infrastructure/src/main/java/com/academy/orders/infrastructure/order/repository/OrderRepositoryImpl.java
+++ code/orders-infrastructure/src/main/java/com/academy/orders/infrastructure/order/repository/OrderRepositoryImpl.java
@@ -26,14 +26,14 @@ public class OrderRepositoryImpl implements OrderRepository {
 
 	@Override
 	public Optional<Order> findById(UUID id) {
-        try {
-            //TODO delete me
-            final var colors = colorsApi.getColors();
-            log.info("Retrieved colors {}", colors);
-        }  catch (Exception e) {
-            log.error("Ops", e);
-        }
-        
+		try {
+			// TODO delete me
+			final var colors = colorsApi.getColors();
+			log.info("Retrieved colors {}", colors);
+		} catch (Exception e) {
+			log.error("Ops", e);
+		}
+
 		return jpaAdapter.findById(id).map(mapper::fromEntity);
 	}
 }
