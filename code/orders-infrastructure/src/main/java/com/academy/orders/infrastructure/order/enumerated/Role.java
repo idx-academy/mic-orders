f4c156a2fc0b7338bff0fe8842568ad677bdcb38
liubchenko_denys <denyslubchenko@gmail.com>
2024-06-18 12:24:14 +0300

    Created account entity

---
+++ code/orders-infrastructure/src/main/java/com/academy/orders/infrastructure/order/enumerated/Role.java
@@ -0,0 +1,5 @@
+package com.academy.orders.infrastructure.order.enumerated;
+
+public enum Role {
+    ROLE_USER
+}
---
+++ code/orders-infrastructure/src/main/java/com/academy/orders/infrastructure/order/enumerated/UserStatus.java
@@ -0,0 +1,6 @@
+package com.academy.orders.infrastructure.order.enumerated;
+
+public enum UserStatus {
+    ACTIVE,
+    DEACTIVATED
+}
