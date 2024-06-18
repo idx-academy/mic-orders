3ff4bbd90d5a11f98dffdc714d74a36808b1d792
liubchenko_denys <denyslubchenko@gmail.com>
2024-06-18 12:03:08 +0300

    Created accounts table migration

---
+++ code/orders-boot/src/main/resources/db/migration/V1__createAccountsTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS "ACCOUNTS"
+(
+    Id         BIGSERIAL CONSTRAINT PK_accounts PRIMARY KEY,
+    password   varchar(255) NOT NULL,
+    email      varchar(100) NOT NULL  CONSTRAINT UQ_accounts_email UNIQUE,
+    first_name varchar(100) NOT NULL,
+    last_name  varchar(100) NOT NULL,
+    role       varchar(20)  NOT NULL,
+    status     varchar(20)  NOT NULL,
+    created_at timestamp
+);
