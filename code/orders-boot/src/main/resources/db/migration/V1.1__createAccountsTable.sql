59ffde4f841a1fe672fc58cec573a57ba426e450
liubchenko_denys <denyslubchenko@gmail.com>
2024-06-18 14:38:07 +0300

    Renamed accounts table to lover case

--- code/orders-boot/src/main/resources/db/migration/V1.1__createAccountsTable.sql
+++ code/orders-boot/src/main/resources/db/migration/V1.1__createAccountsTable.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS "ACCOUNTS"
+CREATE TABLE IF NOT EXISTS "accounts"
 (
-    Id         BIGSERIAL CONSTRAINT PK_accounts PRIMARY KEY,
+    id         BIGSERIAL CONSTRAINT PK_accounts PRIMARY KEY,
     password   varchar(255) NOT NULL,
     email      varchar(100) NOT NULL  CONSTRAINT UQ_accounts_email UNIQUE,
     first_name varchar(100) NOT NULL,
