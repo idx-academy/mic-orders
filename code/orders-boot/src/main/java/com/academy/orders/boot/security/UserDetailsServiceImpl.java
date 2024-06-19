3f29afa2b2199a313ddadccd67b4d068e21a695e
ryhaldenys <122037168+ryhaldenys@users.noreply.github.com>
2024-06-19 15:50:23 +0300

    implemented tests to UserDetailsServiceImpl class

--- code/orders-boot/src/main/java/com/academy/orders/boot/security/UserDetailsServiceImpl.java
+++ code/orders-boot/src/main/java/com/academy/orders/boot/security/UserDetailsServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserDetailServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 	private final AccountRepository accountRepository;
 
 	@Override
