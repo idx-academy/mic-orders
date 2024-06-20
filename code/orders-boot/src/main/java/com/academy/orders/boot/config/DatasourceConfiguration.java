cb015de0ae8b8a912245bae008ee1cacab87cf7c
Anton Bondar <bondar.local@gmail.com>
2024-06-20 13:19:01 +0300

    deleted unused imports

--- code/orders-boot/src/main/java/com/academy/orders/boot/config/DatasourceConfiguration.java
+++ code/orders-boot/src/main/java/com/academy/orders/boot/config/DatasourceConfiguration.java
@@ -2,11 +2,8 @@ package com.academy.orders.boot.config;
 
 import java.util.Objects;
 
-import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
