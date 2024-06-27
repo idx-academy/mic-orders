00fcc38d69d0683b28df17cdecc26db8986de950
liubchenko_denys <denyslubchenko@gmail.com>
2024-06-27 18:51:32 +0300

    Added domains and generic mappers for pagination

---
+++ code/orders-infrastructure/src/main/java/com/academy/orders/infrastructure/common/PageableMapper.java
@@ -0,0 +1,31 @@
+package com.academy.orders.infrastructure.common;
+
+import com.academy.orders.domain.common.Pageable;
+import java.util.List;
+import org.mapstruct.Mapper;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+@Mapper(componentModel = "spring")
+public interface PageableMapper {
+	default PageRequest fromDomain(Pageable pageable) {
+		if (pageable == null) {
+			return null;
+		}
+
+		Sort sort = null;
+
+		sort = map(pageable.sort());
+
+		int pageNumber = pageable.page();
+		int pageSize = pageable.size();
+
+		PageRequest pageRequest = PageRequest.of(pageNumber, pageSize, sort);
+
+		return pageRequest;
+	}
+
+	default Sort map(List<String> value) {
+		return Sort.by(value.toArray(new String[0]));
+	}
+}
