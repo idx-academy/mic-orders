2283a74175251f0f097bc72017259c1781ec61bd
ryhaldenys <122037168+ryhaldenys@users.noreply.github.com>
2024-06-29 18:47:41 +0300

    implemented test cases.

--- code/orders-application/src/test/java/com/academy/orders/application/cart/usecase/CreateCartItemByUserUseCaseUseCaseTest.java
+++ code/orders-application/src/test/java/com/academy/orders/application/cart/usecase/CreateCartItemByUserUseCaseUseCaseTest.java
@@ -3,9 +3,12 @@ package com.academy.orders.application.cart.usecase;
 import com.academy.orders.domain.account.entity.Account;
 import com.academy.orders.domain.cart.entity.CartItem;
 import com.academy.orders.domain.cart.entity.CreateCartItemDTO;
-import com.academy.orders.domain.cart.exception.CartItemAlreadyExistsException;
 import com.academy.orders.domain.cart.repository.CartItemRepository;
 import com.academy.orders.domain.product.entity.Product;
+import com.academy.orders.domain.product.exception.ProductNotFoundException;
+import com.academy.orders.domain.product.repository.ProductRepository;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,52 +17,73 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static com.academy.orders.application.ModelUtils.getAccount;
 import static com.academy.orders.application.ModelUtils.getProduct;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CreateCartItemByUserUseCaseUseCaseTest {
-	@Mock
-	private CartItemRepository cartItemRepository;
 	@InjectMocks
 	private CreateCartItemByUserUseCaseUseCaseImpl createCartItemByUserUseCase;
+	@Mock
+	private CartItemRepository cartItemRepository;
+	@Mock
+	private ProductRepository productRepository;
 
-	@Test
-	void createCartItemByUserTest() {
-		Account account = getAccount();
-		Product product = getProduct();
+	private CreateCartItemDTO cartItemDTO;
+	private Product product;
+	private Account account;
 
-		int quantity = 1;
+	@BeforeEach
+	void setUp() {
+		account = getAccount();
+		product = getProduct();
 
-		CreateCartItemDTO createCartItemDTO = CreateCartItemDTO.builder().productId(product.id()).userId(account.id())
-				.quantity(quantity).build();
-		CartItem cartItem = CartItem.builder().product(product).account(account).quantity(quantity).build();
-
-		when(cartItemRepository.existsByProductIdAndUserId(createCartItemDTO.productId(), createCartItemDTO.userId()))
-				.thenReturn(false);
-		when(cartItemRepository.save(createCartItemDTO)).thenReturn(cartItem);
-
-		createCartItemByUserUseCase.create(createCartItemDTO);
-
-		verify(cartItemRepository).existsByProductIdAndUserId(createCartItemDTO.productId(),
-				createCartItemDTO.userId());
-		verify(cartItemRepository).save(createCartItemDTO);
+		cartItemDTO = CreateCartItemDTO.builder().productId(product.id()).userId(account.id()).quantity(1).build();
 	}
 
 	@Test
-	void createThrowsCartItemAlreadyExistsExceptionTest() {
-		Account account = getAccount();
-		Product product = getProduct();
+	void testCreateCartItemByUserIfProductIsNotAddedToTheCart() {
+		var cartItem = CartItem.builder().product(product).account(account).quantity(1).build();
 
-		int quantity = 1;
+		when(productRepository.existById(product.id())).thenReturn(true);
+		when(cartItemRepository.existsByProductIdAndUserId(cartItemDTO.productId(), cartItemDTO.userId()))
+				.thenReturn(false);
+		when(cartItemRepository.save(cartItemDTO)).thenReturn(cartItem);
 
-		CreateCartItemDTO createCartItemDTO = CreateCartItemDTO.builder().productId(product.id()).userId(account.id())
-				.quantity(quantity).build();
+		assertDoesNotThrow(() -> createCartItemByUserUseCase.create(cartItemDTO));
 
-		when(cartItemRepository.existsByProductIdAndUserId(createCartItemDTO.productId(), createCartItemDTO.userId()))
+		verify(productRepository).existById(any(UUID.class));
+		verify(cartItemRepository).existsByProductIdAndUserId(any(UUID.class), anyLong());
+		verify(cartItemRepository).save(cartItemDTO);
+	}
+
+	@Test
+	void testCreateCartItemByUserIfProductIsAddedToTheCart() {
+		when(productRepository.existById(product.id())).thenReturn(true);
+		when(cartItemRepository.existsByProductIdAndUserId(cartItemDTO.productId(), cartItemDTO.userId()))
 				.thenReturn(true);
+		doNothing().when(cartItemRepository).incrementQuantity(product.id(), account.id());
 
-		assertThrows(CartItemAlreadyExistsException.class, () -> createCartItemByUserUseCase.create(createCartItemDTO));
+		assertDoesNotThrow(() -> createCartItemByUserUseCase.create(cartItemDTO));
+
+		verify(productRepository).existById(any(UUID.class));
+		verify(cartItemRepository).existsByProductIdAndUserId(any(UUID.class), anyLong());
+		verify(cartItemRepository).incrementQuantity(any(UUID.class), anyLong());
+		verify(cartItemRepository, never()).save(cartItemDTO);
+
+	}
+
+	@Test
+	void testCreateCartItemByUserIfProductDoesNotExists() {
+		when(productRepository.existById(product.id())).thenReturn(false);
+
+		assertThrowsExactly(ProductNotFoundException.class, () -> createCartItemByUserUseCase.create(cartItemDTO));
+		verify(productRepository).existById(any(UUID.class));
 	}
 }
