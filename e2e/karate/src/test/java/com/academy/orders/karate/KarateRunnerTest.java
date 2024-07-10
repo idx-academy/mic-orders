87818e1a50a8f2ab2b1e630d63fc745ab516bb42
Denys <122037168+ryhaldenys@users.noreply.github.com>
2024-07-10 13:45:57 +0300

    added karate tests to cart endpoints and cucumber report (#146)

--- e2e/karate/src/test/java/com/academy/orders/karate/KarateRunnerTest.java
+++ e2e/karate/src/test/java/com/academy/orders/karate/KarateRunnerTest.java
@@ -1,18 +1,36 @@
 package com.academy.orders.karate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import com.intuit.karate.Runner;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 
-public class KarateRunnerTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class KarateRunnerTest {
 
   @Test
   void triggerKarate() {
     var results = Runner.builder()
         .path("classpath:apis")
+        .outputCucumberJson(true)
         .parallel(1);
 
-   assertEquals(0, results.getFailCount(), results.getErrorMessages());
+      generateReport(results.getReportDir());
+      assertEquals(0, results.getFailCount(), results.getErrorMessages());
+  }
+
+  static void generateReport(String karateOutputPath) {
+      Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+      List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+      jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+      Configuration config = new Configuration(new File("target"), "Retail");
+      ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+      reportBuilder.generateReports();
   }
 }
