2143b2423b5de95311d645781b763f4c3576e8ac
ryhaldenys <122037168+ryhaldenys@users.noreply.github.com>
2024-07-26 11:42:27 +0300

    added test

--- code/orders-boot/src/test/java/com/academy/orders/boot/infrastructure/common/AbstractRepository.java
+++ code/orders-boot/src/test/java/com/academy/orders/boot/infrastructure/common/AbstractRepository.java
@@ -2,11 +2,7 @@ package com.academy.orders.boot.infrastructure.common;
 
 import com.academy.orders.boot.Application;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
