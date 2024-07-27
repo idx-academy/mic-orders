f2733af81e5ccf16aa69a474841bf42e359f3f84
Anton Bondar <53192419+ABbondar@users.noreply.github.com>
2024-07-27 17:20:58 +0300

    edited logic for update product controller (#222)

--- code/orders-application/src/test/java/com/academy/orders/application/product/usecase/UpdateProductUseCaseImplTest.java
+++ code/orders-application/src/test/java/com/academy/orders/application/product/usecase/UpdateProductUseCaseImplTest.java
@@ -1,5 +1,7 @@
 package com.academy.orders.application.product.usecase;
 
+import com.academy.orders.domain.language.repository.LanguageRepository;
+import com.academy.orders.domain.language.repository.exception.LanguageNotFoundException;
 import com.academy.orders.domain.product.entity.ProductManagement;
 import com.academy.orders.domain.product.exception.ProductNotFoundException;
 import com.academy.orders.domain.product.repository.ProductRepository;
@@ -11,6 +13,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 import java.util.Set;
+
+import static com.academy.orders.application.ModelUtils.getEmptyUpdateProduct;
+import static com.academy.orders.application.ModelUtils.getLanguage;
+import static com.academy.orders.application.ModelUtils.getProductManagement;
 import static com.academy.orders.application.ModelUtils.getProductTranslationManagement;
 import static com.academy.orders.application.ModelUtils.getTag;
 import static com.academy.orders.application.ModelUtils.getUpdateProduct;
@@ -32,30 +38,78 @@ class UpdateProductUseCaseImplTest {
 	@Mock
 	private TagRepository tagRepository;
 
+	@Mock
+	private LanguageRepository languageRepository;
+
 	@Test
 	void updateProductTest() {
 		var updateProduct = getUpdateProduct();
+		var productManagement = getProductManagement();
 		var productTranslationManagement = getProductTranslationManagement();
+		var language = getLanguage();
 
 		when(productRepository.existById(TEST_UUID)).thenReturn(true);
+		when(languageRepository.findByCode(LANGUAGE_EN)).thenReturn(language);
 		when(tagRepository.getTagsByIds(List.of(TEST_ID))).thenReturn(Set.of(getTag()));
-
-		when(productRepository.findByIdAndLanguageCode(TEST_UUID, LANGUAGE_EN))
+		when(productRepository.findTranslationByIdAndLanguageCode(TEST_UUID, LANGUAGE_EN))
 				.thenReturn(productTranslationManagement);
+		when(productRepository.findProductByIdAndLanguageCode(TEST_UUID, LANGUAGE_EN)).thenReturn(productManagement);
+
 		doNothing().when(productRepository).update(any(ProductManagement.class));
 
 		updateProductUseCase.updateProduct(TEST_UUID, LANGUAGE_EN, updateProduct);
 
 		verify(productRepository).existById(TEST_UUID);
-		verify(productRepository).findByIdAndLanguageCode(TEST_UUID, LANGUAGE_EN);
+		verify(languageRepository).findByCode(LANGUAGE_EN);
+		verify(tagRepository).getTagsByIds(List.of(TEST_ID));
+		verify(productRepository).findTranslationByIdAndLanguageCode(TEST_UUID, LANGUAGE_EN);
+		verify(productRepository).findProductByIdAndLanguageCode(TEST_UUID, LANGUAGE_EN);
 	}
 
 	@Test
-	void updateProductThrowNotFoundExceptionTest() {
+	void updateProductWithEmptyRequestTest() {
+		var updateProduct = getEmptyUpdateProduct();
+		var productManagement = getProductManagement();
+		var productTranslationManagement = getProductTranslationManagement();
+		var language = getLanguage();
+
+		when(productRepository.existById(TEST_UUID)).thenReturn(true);
+		when(languageRepository.findByCode(LANGUAGE_EN)).thenReturn(language);
+		when(tagRepository.getTagsByIds(List.of(TEST_ID))).thenReturn(Set.of(getTag()));
+		when(productRepository.findTranslationByIdAndLanguageCode(TEST_UUID, LANGUAGE_EN))
+				.thenReturn(productTranslationManagement);
+		when(productRepository.findProductByIdAndLanguageCode(TEST_UUID, LANGUAGE_EN)).thenReturn(productManagement);
+
+		doNothing().when(productRepository).update(any(ProductManagement.class));
+
+		updateProductUseCase.updateProduct(TEST_UUID, LANGUAGE_EN, updateProduct);
+
+		verify(productRepository).existById(TEST_UUID);
+		verify(languageRepository).findByCode(LANGUAGE_EN);
+		verify(tagRepository).getTagsByIds(List.of(TEST_ID));
+		verify(productRepository).findTranslationByIdAndLanguageCode(TEST_UUID, LANGUAGE_EN);
+		verify(productRepository).findProductByIdAndLanguageCode(TEST_UUID, LANGUAGE_EN);
+	}
+
+	@Test
+	void updateProductThrowsProductNotFoundExceptionTest() {
 		var updateProduct = getUpdateProduct();
 		when(productRepository.existById(TEST_UUID)).thenReturn(false);
 		assertThrows(ProductNotFoundException.class,
 				() -> updateProductUseCase.updateProduct(TEST_UUID, LANGUAGE_EN, updateProduct));
 		verify(productRepository).existById(TEST_UUID);
+	}
+
+	@Test
+	void updateProductThrowsLanguageNotFoundExceptionTest() {
+		var updateProduct = getUpdateProduct();
+		when(productRepository.existById(TEST_UUID)).thenReturn(true);
+		when(languageRepository.findByCode(LANGUAGE_EN)).thenReturn(null);
+
+		assertThrows(LanguageNotFoundException.class,
+				() -> updateProductUseCase.updateProduct(TEST_UUID, LANGUAGE_EN, updateProduct));
+
+		verify(productRepository).existById(TEST_UUID);
+		verify(languageRepository).findByCode(LANGUAGE_EN);
 	}
 }
--- code/orders-infrastructure/src/main/java/com/academy/orders/infrastructure/product/repository/ProductJpaAdapter.java
+++ code/orders-infrastructure/src/main/java/com/academy/orders/infrastructure/product/repository/ProductJpaAdapter.java
@@ -133,6 +133,21 @@ public interface ProductJpaAdapter extends JpaRepository<ProductEntity, UUID> {
 	void updateProductStatus(UUID id, ProductStatus status);
 
 	/**
+	 * Finds a product by its ID and language code.
+	 *
+	 * @param id
+	 *            the ID of the product.
+	 * @param languageCode
+	 *            the language code for filtering the product translation.
+	 * @return the {@link ProductEntity} object.
+	 *
+	 * @author Anton Bondar
+	 */
+	@Query("SELECT p FROM ProductEntity p LEFT JOIN FETCH p.productTranslations pt "
+			+ "LEFT JOIN FETCH pt.language l LEFT JOIN FETCH p.tags t WHERE p.id = :id AND l.code = :languageCode")
+	ProductEntity findProductByIdAndLanguageCode(UUID id, String languageCode);
+
+	/**
 	 * Finds a product translation by product ID and language code.
 	 *
 	 * @param id
@@ -145,5 +160,5 @@ public interface ProductJpaAdapter extends JpaRepository<ProductEntity, UUID> {
 	 */
 	@Query("SELECT pt FROM ProductTranslationEntity pt LEFT JOIN FETCH pt.product p "
 			+ "LEFT JOIN FETCH pt.language l LEFT JOIN FETCH p.tags t WHERE p.id = :id AND l.code = :languageCode")
-	ProductTranslationEntity findByIdAndLanguageCode(UUID id, String languageCode);
+	ProductTranslationEntity findTranslationByIdAndLanguageCode(UUID id, String languageCode);
 }
