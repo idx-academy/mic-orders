8dfa4fb3c1f8e13672b84d22da61a6a7434038e8
Anton Bondar <53192419+ABbondar@users.noreply.github.com>
2024-08-06 00:04:52 +0300

    Test/278/Add integration tests to product repository (#279)
    * added tests to product repository
    
    * issues

---
+++ code/orders-boot/src/test/java/com/academy/orders/boot/TestConstants.java
@@ -0,0 +1,15 @@
+package com.academy.orders.boot;
+
+import java.util.UUID;
+
+public class TestConstants {
+	public static final UUID PRODUCT_UUID = UUID.fromString("3c6c4db3-08c3-4473-a3a4-7d91100ff36a");
+	public static final String PRODUCT_IMAGE = "https://j65jb0fdkxuua0go.public.blob.vercel-storage.com/phone_2-tTDYhyoyqsEkwPzySFdXflYCe7TkUb.jpg";
+	public static final int TOTAL_ELEMENTS_PRODUCTS = 34;
+	public static final int TOTAL_ELEMENTS_COMPUTERS = 16;
+	public static final String TAG_COMPUTERS = "category:computer";
+	public static final String SORT_BY_PRICE_ASC = "price,asc";
+	public static final String SORT_BY_PRICE_DESC = "price,desc";
+	public static final String LANGUAGE_UK = "uk";
+	public static final int NUMBER_OF_TRANSLATIONS_UK_AND_EN = 2;
+}
