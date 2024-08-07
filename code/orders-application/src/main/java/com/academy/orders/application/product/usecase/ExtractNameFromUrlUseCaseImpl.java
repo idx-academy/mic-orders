107f579da94d945541853e0c2eee6500d4de590a
ryhaldenys <122037168+ryhaldenys@users.noreply.github.com>
2024-08-07 15:55:33 +0300

    added tests to ExtractNameFromUrlUseCaseImpl class

--- code/orders-application/src/main/java/com/academy/orders/application/product/usecase/ExtractNameFromUrlUseCaseImpl.java
+++ code/orders-application/src/main/java/com/academy/orders/application/product/usecase/ExtractNameFromUrlUseCaseImpl.java
@@ -3,10 +3,12 @@ package com.academy.orders.application.product.usecase;
 import com.academy.orders.domain.product.usecase.ExtractNameFromUrlUseCase;
 import org.springframework.stereotype.Service;
 
+import static java.util.Objects.nonNull;
+
 @Service
 public class ExtractNameFromUrlUseCaseImpl implements ExtractNameFromUrlUseCase {
 	@Override
 	public String extractNameFromUrl(String url) {
-		return url.substring(url.lastIndexOf('/') + 1);
+		return nonNull(url) ? url.substring(url.lastIndexOf('/') + 1) : null;
 	}
 }
