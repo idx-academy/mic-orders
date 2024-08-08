f4b7c004ac2437361378211c679db6c940e6661e
ryhaldenys <122037168+ryhaldenys@users.noreply.github.com>
2024-08-08 17:15:07 +0300

    added javadoc to Order repositories and usecases

--- code/orders-domain/src/main/java/com/academy/orders/domain/order/repository/OrderImageRepository.java
+++ code/orders-domain/src/main/java/com/academy/orders/domain/order/repository/OrderImageRepository.java
@@ -3,5 +3,15 @@ package com.academy.orders.domain.order.repository;
 import com.academy.orders.domain.order.entity.Order;
 
 public interface OrderImageRepository {
+	/**
+	 * Loads the images for the products associated with the given order.
+	 *
+	 * @param order
+	 *            the {@link Order} for which the products images needs to be
+	 *            loaded.
+	 * @return the updated {@link Order} with the products images loaded.
+	 *
+	 * @author Denys Ryhal
+	 */
 	Order loadImageForProductInOrder(Order order);
 }
--- code/orders-domain/src/main/java/com/academy/orders/domain/order/usecase/GetOrderByIdUseCase.java
+++ code/orders-domain/src/main/java/com/academy/orders/domain/order/usecase/GetOrderByIdUseCase.java
@@ -1,10 +1,24 @@
 package com.academy.orders.domain.order.usecase;
 
+import com.academy.orders.domain.order.exception.OrderNotFoundException;
 import java.util.UUID;
 
 import com.academy.orders.domain.order.entity.Order;
 
 public interface GetOrderByIdUseCase {
-
+	/**
+	 * Retrieves an order by its ID with details localized to the specified
+	 * language.
+	 *
+	 * @param id
+	 *            the UUID of the order to be retrieved.
+	 * @param language
+	 *            the language code for localization of the order details.
+	 * @return the {@link Order} with localized details if found.
+	 * @throws OrderNotFoundException
+	 *             if no order with the specified ID is found.
+	 *
+	 * @author Denys Liubchenko
+	 */
 	Order getOrderById(UUID id, String language);
 }
