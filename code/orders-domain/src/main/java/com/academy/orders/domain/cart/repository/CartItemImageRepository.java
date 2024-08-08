83c0dfd8d60df8d3c2cf0e88ece161ca2efc5548
ryhaldenys <122037168+ryhaldenys@users.noreply.github.com>
2024-08-08 17:08:44 +0300

    added javadoc to Cart usecases and repositories

--- code/orders-domain/src/main/java/com/academy/orders/domain/cart/repository/CartItemImageRepository.java
+++ code/orders-domain/src/main/java/com/academy/orders/domain/cart/repository/CartItemImageRepository.java
@@ -2,6 +2,21 @@ package com.academy.orders.domain.cart.repository;
 
 import com.academy.orders.domain.cart.entity.CartItem;
 
+/**
+ * Repository interface for managing and loading images for products in a
+ * shopping cart.
+ */
 public interface CartItemImageRepository {
+
+	/**
+	 * Loads the image for the product associated with the given cart item.
+	 *
+	 * @param cartItem
+	 *            the {@link CartItem} for which the product image needs to be
+	 *            loaded.
+	 * @return the updated {@link CartItem} with the product image loaded.
+	 *
+	 * @author Denys Ryhal
+	 */
 	CartItem loadImageForProductInCart(CartItem cartItem);
 }
--- code/orders-domain/src/main/java/com/academy/orders/domain/cart/usecase/SetCartItemQuantityUseCase.java
+++ code/orders-domain/src/main/java/com/academy/orders/domain/cart/usecase/SetCartItemQuantityUseCase.java
@@ -5,5 +5,20 @@ import com.academy.orders.domain.cart.dto.UpdatedCartItemDto;
 import java.util.UUID;
 
 public interface SetCartItemQuantityUseCase {
+	/**
+	 * Sets the quantity of the specified product in the user's shopping cart.
+	 *
+	 * @param productId
+	 *            the UUID of the product whose quantity is to be updated in the
+	 *            cart.
+	 * @param userId
+	 *            the ID of the user whose cart item quantity is to be updated.
+	 * @param quantity
+	 *            the new quantity to set for the product in the cart.
+	 * @return an {@link UpdatedCartItemDto} containing the updated cart item
+	 *         details.
+	 *
+	 * @author Yurii Osovskyi
+	 */
 	UpdatedCartItemDto setQuantity(UUID productId, Long userId, Integer quantity);
 }
